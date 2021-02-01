966f0991c3c97dff23b11cdf38f8a3817e6839ee
Tim R <tim.ribaric@gmail.com>
2021-02-01 09:58:41 -0500

    automatic pull requests

---
+++ p2_prep_and_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Part 2 of Workshop"
+
+#clone from list antendees and switch to main branch
+pull_list="list_of_clones.txt"
+while IFS= read -r line;
+do
+  git clone $line
+done < $pull_list
+
+
+startdir=$(pwd)
+#touch analysis.py in each directory to change values
+for d in $(ls -d **/)
+do
+  cd $startdir"/"$d
+  #modify each file
+  sed -i '' 's/mean/median/g' analyze.py
+  git add analyze.py
+  git commit -m "new average method"
+done
